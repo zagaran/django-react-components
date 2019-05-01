225f9b7d32d443d719a5985576e575035a3055ba
Alvin Siu <alvin@zagaran.com>
2019-05-01 19:04:06 -0400

    WIP refactor react_component to escape json-compliant objects recursively

--- django_react_loader/templatetags/django_react_loader.py
+++ django_react_loader/templatetags/django_react_loader.py
@@ -1,19 +1,52 @@
 import json
+import uuid
 
 from django import template
-from django.utils.html import format_html
+from django.utils.html import conditional_escape
 from django.utils.safestring import mark_safe
 
 register = template.Library()
 
 
 @register.simple_tag
-def react_component(comp, **kwargs):
-    if 'id' in kwargs:
-        html_id = kwargs['id']
+def react_component(component_name, **kwargs):
+    """
+    Render a React component with kwargs as attributes. This current requires that all kwargs
+    being passed in be JSON-serializable.
+    """
+    component_name = conditional_escape_json(component_name)
+    kwargs['id'] = kwargs.get('id', str(uuid.uuid4()))
+    
+    kwargs_safe = mark_safe(
+        json.dumps(
+            {conditional_escape_json(key): conditional_escape_json(value)
+             for key, value in kwargs.items()}
+        )
+    )
+    react_component_html = f"""
+        <span>1</span>
+        <div id="{kwargs['id']}"></div>
+        <script type="text/javascript">
+            window.reactComponents.{component_name}.init('{kwargs_safe}')
+            window.reactComponents.{component_name}.render()
+        </script>
+    """
+    return mark_safe(react_component_html)
+
+
+def conditional_escape_json(obj):
+    """
+    Conditionally escape json-like object such that it retains its data type and will only run
+    mark_safe on strings.
+    """
+    if isinstance(obj, dict):
+        return {
+            conditional_escape_json(key): conditional_escape_json(value)
+            for key, value in obj.items()
+        }
+    elif isinstance(obj, list):
+        return [conditional_escape_json(item) for item in obj]
+    elif isinstance(obj, str):
+        return conditional_escape(obj)
     else:
-        html_id = comp
-        kwargs['id'] = comp
-    props = json.dumps(kwargs).replace("</", "<\\/").replace("\\", "\\\\")
-    response = format_html("<div id='{html_id}'></div><script type='text/javascript'>window.reactComponents.{comp}.init('{props}');window.reactComponents.{comp}.render()</script>", html_id=html_id, comp=comp, props=mark_safe(props))
-    return response
+        return obj
