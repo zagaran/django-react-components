2c03eab99991f670314f878202aa1a1a590a302f
Alvin Siu <alvin@zagaran.com>
2019-05-01 15:45:20 -0400

    Configure package

---
+++ django_react_loader/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DjangoReactLoaderConfig(AppConfig):
+    name = 'django_react_loader'
---
+++ setup.py
@@ -0,0 +1,43 @@
+import os
+from setuptools import find_packages, setup
+
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# Allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    author='Zagaran, Inc.',
+    author_email='info@zagaran.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
+        'Framework :: Django :: 2.2',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: User Interfaces',
+    ],
+    description='A collection of tools that assist with loading and rendering React components',
+    license='MIT',
+    long_description=README,
+    name='django-react-loader',
+    packages=find_packages(),
+    url='https://github.com/zagaran/django-react-loader',
+    version='0.1.0a1',
+)
