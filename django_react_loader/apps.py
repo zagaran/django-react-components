f96520c162d4437c9a98ba8877d02d22431a0f91
Alvin Siu <alvin@zagaran.com>
2019-05-01 16:35:39 -0400

    Implement dependency checks

--- django_react_loader/apps.py
+++ django_react_loader/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class DjangoReactLoaderConfig(AppConfig):
     name = 'django_react_loader'
+    
+    def ready(self):
+        # Add System checks
+        from django_react_loader.checks import dependency_checks
