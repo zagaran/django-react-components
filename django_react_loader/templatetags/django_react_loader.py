bb7f178fad285608fad4d2a7af9ed1e0528b47df
Alvin Siu <alvin@zagaran.com>
2019-05-02 09:58:34 -0400

    Reformat and add comments to react_component for clarity

--- django_react_loader/templatetags/django_react_loader.py
+++ django_react_loader/templatetags/django_react_loader.py
@@ -1,19 +1,36 @@
+"""
+Template tags for django-react-loader>>
+"""
 import json
+import uuid
 
 from django import template
-from django.utils.html import format_html, strip_tags, escapejs
+from django.utils.html import format_html
 from django.utils.safestring import mark_safe
 
 register = template.Library()
 
 
 @register.simple_tag
-def react_component(comp, **kwargs):
-    if 'id' in kwargs:
-        html_id = kwargs['id']
-    else:
-        html_id = comp
-        kwargs['id'] = comp
-    props = escapejs(strip_tags(json.dumps(kwargs)))
-    response = format_html("<div id='{html_id}'></div><script type='text/javascript'>window.reactComponents.{comp}.init('{props}');window.reactComponents.{comp}.render()</script>", html_id=html_id, comp=comp, props=mark_safe(props))
-    return response
+def react_component(component_name, **kwargs):
+    """
+    Render a React component with kwargs as attributes. This current requires that all kwargs
+    being passed in be JSON-serializable.
+    """
+    kwargs['id'] = kwargs.get('id', str(uuid.uuid4()))
+    props = json.dumps(kwargs).replace("</", "<\\/").replace("\\", "\\\\")
+    
+    react_component_html = """
+        <div id="{html_id}"></div>
+        <script type="text/javascript">
+            window.reactComponents.{component_name}.init('{props}')
+            window.reactComponents.{component_name}.render()
+        </script>
+    """
+    
+    return format_html(
+        react_component_html,
+        props=mark_safe(props),
+        html_id=kwargs['id'],
+        component_name=component_name,
+    )
